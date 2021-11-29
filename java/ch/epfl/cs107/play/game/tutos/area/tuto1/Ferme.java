8a568c3fd6cba405d2ffd868b4bb1e8462d40d22
Arthur Wolf <arthur.wlf@icloud.com>
2021-11-29 18:30:16 +0100

    started tuto2, not finished though

--- java/ch/epfl/cs107/play/game/tutos/area/tuto1/Ferme.java
+++ java/ch/epfl/cs107/play/game/tutos/area/tuto1/Ferme.java
@@ -1,10 +1,13 @@
 package ch.epfl.cs107.play.game.tutos.area.tuto1;
 
+import ch.epfl.cs107.play.game.areagame.actor.Background;
 import ch.epfl.cs107.play.game.tutos.area.SimpleArea;
 
 public class Ferme extends SimpleArea {
 
-    public void createArea(){}
+    public void createArea(){
+        registerActor(new Background(this));
+    }
 
     @Override
     public String getTitle() {
--- java/ch/epfl/cs107/play/game/tutos/area/tuto1/Village.java
+++ java/ch/epfl/cs107/play/game/tutos/area/tuto1/Village.java
@@ -1,5 +1,6 @@
 package ch.epfl.cs107.play.game.tutos.area.tuto1;
 
+import ch.epfl.cs107.play.game.areagame.actor.Background;
 import ch.epfl.cs107.play.game.tutos.actor.SimpleGhost;
 import ch.epfl.cs107.play.game.tutos.area.SimpleArea;
 import ch.epfl.cs107.play.math.Vector;
@@ -7,6 +8,7 @@ import ch.epfl.cs107.play.math.Vector;
 public class Village extends SimpleArea {
     public void createArea(){
         registerActor(new SimpleGhost(new Vector(18, 7), "ghost.2"));
+        registerActor(new Background(this));
     }
 
     @Override
